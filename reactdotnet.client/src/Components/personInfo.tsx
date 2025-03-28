92d59dfda58148e2e9d0ca4dc83b1c7b6ff42c1a
Riku Tomann <riku.tomann@seamk.fi>
2025-03-28 15:12:34 +0200

    css, tailwind, index

--- reactdotnet.client/src/Components/personInfo.tsx
+++ reactdotnet.client/src/Components/personInfo.tsx
@@ -2,19 +2,19 @@ import Person from "../types/Person"
 
 const PersonInfo = ({persons, del}: {persons: Person, del: any}) => {
   return (
-    <table className="table-auto align-middle">
-  <thead>
+    <table className="w-full text-sm text-left rtl:text-right">
+  <thead className="text-xl uppercase text-center text-orange-400">
     <tr>
-      <th>Name</th>
+      <th >Name</th>
       <th>Surname</th>
       <th>Age</th>
     </tr>
   </thead>
-  <tbody>
-    <tr>
-      <td>{persons.Name}</td>
-      <td>{persons.Surname}</td>
-      <td>{persons.Age}</td>
+  <tbody >
+    <tr >
+      <td className="bg-gray-700 text-center">{persons.Name}</td>
+      <td className="bg-gray-700 text-center">{persons.Surname}</td>
+      <td className="bg-gray-700 text-center">{persons.Age}</td>
       <td>
         <button onClick={() => del(persons.Id)}>Delete</button>
       </td>
